51bbea0b1d81ada2701304d2d59260c95596e97c
ujikawa <ujikawa1026@gmail.com>
2018-04-14 08:07:21 +0900

    とりあえずのアニメーション

--- sample-shared-element-transition/app/src/main/java/com/ujikawa/sample1/MainActivity.java
+++ sample-shared-element-transition/app/src/main/java/com/ujikawa/sample1/MainActivity.java
@@ -1,8 +1,10 @@
 package com.ujikawa.sample1;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -15,13 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        final Activity activity = this;
 
         binding.baby.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, NextActivity.class);
-                startActivity(intent);
+                final Intent intent = new Intent(MainActivity.this, NextActivity.class);
+                final ActivityOptionsCompat options = ActivityOptionsCompat
+                        .makeSceneTransitionAnimation(activity, binding.baby, "shared_element_image");
+
+                startActivity(intent, options.toBundle());
             }
         });
     }
--- sample-shared-element-transition/app/src/main/java/com/ujikawa/sample1/NextActivity.java
+++ sample-shared-element-transition/app/src/main/java/com/ujikawa/sample1/NextActivity.java
@@ -1,7 +1,11 @@
 package com.ujikawa.sample1;
 
+import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.ujikawa.sample1.databinding.ActivityNextBinding;
 
 public class NextActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        final ActivityNextBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_next);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            binding.baby.setTransitionName("shared_element_image");
+        }
     }
 }
