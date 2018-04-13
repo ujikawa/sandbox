80bc008a7e62a6f4bda3140b68e470cef298f9fe
ujikawa <hiromitsu.ujikawa@pepabo.com>
2018-04-13 13:50:38 +0900

    画面遷移する最低限のサンプル

--- sample-shared-element-transition/app/src/main/java/com/ujikawa/sample1/MainActivity.java
+++ sample-shared-element-transition/app/src/main/java/com/ujikawa/sample1/MainActivity.java
@@ -1,7 +1,12 @@
 package com.ujikawa.sample1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.ujikawa.sample1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NextActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
