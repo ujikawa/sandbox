a56ae2ed267e07f9d13a349fcf373361c96fcf85
ujikawa <ujikawa1026@gmail.com>
2018-04-22 22:05:40 +0900

    画面遷移

--- react-navigation-lesson/App.js
+++ react-navigation-lesson/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   StyleSheet,
+  Button,
   Text,
   View,
 } from 'react-native';
@@ -15,14 +16,49 @@ const styles = StyleSheet.create({
   },
 });
 
-const AppScreen = () => (
+const HomeScreen = ({ navigation }) => (
   <View style={styles.container}>
     <Text>test</Text>
+    <Button
+      title="Go to Details"
+      onPress={() => navigation.navigate('Details')}
+    />
   </View>
 );
 
-export default StackNavigator({
-  App: {
-    screen: AppScreen,
+const DetailsScreen = ({ navigation }) => (
+  <View style={styles.container}>
+    <Text>Details Screen</Text>
+    <Button
+      title="Go to Details... again"
+      onPress={() => navigation.navigate('Details')}
+    />
+  </View>
+);
+
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        headerTitle: 'Home',
+      },
+    },
+    Details: {
+      screen: DetailsScreen,
+      navigationOptions: {
+        headerTitle: 'Detail',
+      },
+    },
   },
-});
+  {
+    initialRouteName: 'Home',
+  },
+);
+
+const App = () => (
+  <RootStack />
+);
+
+export default App;
