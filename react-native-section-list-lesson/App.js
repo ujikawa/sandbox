db0dcc87d688fe4d8696ff16ace2c66e06aec51c
ujikawa <ujikawa1026@gmail.com>
2018-05-01 22:32:46 +0900

    こんな感じで

--- react-native-section-list-lesson/App.js
+++ react-native-section-list-lesson/App.js
@@ -6,35 +6,56 @@ import {
   View,
 } from 'react-native';
 
-import imageUrl from './poney.png';
+import imageUrl from './favicons.png';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1, // 画面いっぱいに広がる
     backgroundColor: '#F5FCFF',
-    padding: 20,
+    paddingTop: 40,
+    paddingLeft: 20,
   },
   section: {
     flexDirection: 'row', // 横に並べるようになる
     justifyContent: 'flex-start', // 親コンポーネント内の子コンポーネントの配置(縦軸の)を決める。中央から配置される
+    padding: 5,
     alignItems: 'center', // 子コンポーネントの配置(横軸の)を決める。真ん中に配置される
-    margin: 5,
     backgroundColor: 'white',
-    borderBottomWidth: 1,
   },
   image: {
-    margin: 5,
+    padding: 2,
+    height: 24,
+    width: 24,
+    borderRadius: 12,
+    marginRight: 8,
+    borderWidth: 1,
+    borderColor: '#BDBDBD',
+    alignSelf: 'center',
+    resizeMode: 'contain',
   },
   text: {
-    fontSize: 16,
+    justifyContent: 'flex-start',
   },
+  title: {
+    fontSize: 28,
+    color: '#212121',
+    marginBottom: 2,
+  },
+  url: {
+    fontSize: 14,
+    color: '#757575',
+  },
+
 });
 
 const App = () => (
   <View style={styles.container}>
     <View style={styles.section}>
       <Image style={styles.image} source={imageUrl} />
-      <Text style={styles.text}>test</Text>
+      <View style={styles.text}>
+        <Text style={styles.title}>Poney</Text>
+        <Text style={styles.url}>https://www.poney.jp/</Text>
+      </View>
     </View>
   </View>
 );
