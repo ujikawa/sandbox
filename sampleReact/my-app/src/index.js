d7e697d7ce9e4ce2b284a3549f6a9e399792e185
ujikawa <hiromitsu.ujikawa@pepabo.com>
2018-06-05 10:30:44 +0900

    メソッド分け

--- sampleReact/my-app/src/index.js
+++ sampleReact/my-app/src/index.js
@@ -20,8 +20,24 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square
+              value={this.state.squares[i]}
+              onClick={() => this.handleClick(i)}
+            />;
   }
 
   render() {
